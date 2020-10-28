80793a6a045cc1d1a3f821ef728abb463c5cebc8
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-28 15:07:30 +0000

    Changed instantiation to include two players, server and receiver

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,7 @@
 class Game 
 
+  def initialize(server, receiver)
+    @server = server
+    @receiver = receiver
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,17 @@
 require 'game.rb'
 
 describe Game do
+  before(:each) do
+    server = "player 1"
+    receiver = "player 2"
+    @instance = Game.new(server, receiver)
+  end
+
   context '#instantiation' do
     it 'can make an instance of itself' do
-      instance = Game.new
-      expect(instance).to be_a Game
+      expect(@instance).to be_a Game
     end
   end
+
+
 end
